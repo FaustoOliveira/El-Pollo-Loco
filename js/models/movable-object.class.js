5c8bc47700ea47faab24f4e0eebd9daa82f6af1f
Fausto Oliveira <105312418+FaustoOliveira@users.noreply.github.com>
2023-07-27 16:12:37 +0200

    nextUpdate

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-  x = 120;
-  y = 280;
-  img;
-  height = 150;
-  width = 100;
-  imageCache = {};
-  currentImage = 0;
+class MovableObject extends DrawableObject {
   speed = 0.15;
   otherDirection = false;
   speedY = 0;
@@ -25,16 +18,6 @@ class MovableObject {
 
   isAboveGround() {
     return this.y < 151;
-  }
-
-  //loadImage('img/test.png');
-  loadImage(path) {
-    this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
-    this.img.src = path;
-  }
-
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
   drawFrame(ctx) {
@@ -76,20 +59,7 @@ class MovableObject {
   isDead() {
     return this.energy == 0;
   }
-
-  /**
-   *
-   * @param {Arry} arr - ['img/image1.png, 'img/image2.png', ...]
-   */
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      img.style = "transform: scaleX(-1)";
-      this.imageCache[path] = img;
-    });
-  }
-
+  
   playAnimation(images) {
     let i = this.currentImage % images.length; // let i = 7 % 6; => 1, Rest 1
     let path = images[i];
