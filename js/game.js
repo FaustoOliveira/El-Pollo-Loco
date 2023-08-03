a5f49678b4d90aadb87f76b8f3b38c5374b622e4
Fausto Oliveira <105312418+FaustoOliveira@users.noreply.github.com>
2023-08-04 01:09:41 +0200

    uptodate

--- js/game.js
+++ js/game.js
@@ -1,12 +1,84 @@
 let canvas;
 let world;
+let ctx;
+let gameStared = true;
 let keyboard = new Keyboard();
-BACKROUND_AUDIO = new Audio("audio/Background-music.mp3");
+let startScreen = new Image();
+startScreen.src = "img/9_intro_outro_screens/start/startscreen_2.png"
 
 
+/**
+ *  start screen an load 
+ */
+function init() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext('2d');
+  ctx.drawImage(startScreen, 0, 0, 720, 480);
+}
 
-function gameStartScreen(){
- 
+
+function startGame(){
+  gameStared = true;
+  world = new World(canvas, keyboard);
+  document.getElementById('start').style.display = 'none';
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = true;
+  }
+
+  if (e.keyCode == 37) {
+    keyboard.LEFT = true;
+  }
+
+  if (e.keyCode == 38) {
+    keyboard.UP = true;
+  }
+
+  if (e.keyCode == 40) {
+    keyboard.DOWN = true;
+  }
+
+  if (e.keyCode == 32) {
+    keyboard.SPACE = true;
+  }
+
+  if (e.keyCode == 68) {
+    keyboard.D = true;
+  } 
+  
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
+
+  if (e.keyCode == 37) {
+    keyboard.LEFT = false;
+  }
+
+  if (e.keyCode == 38) {
+    keyboard.UP = false;
+  }
+
+  if (e.keyCode == 40) {
+    keyboard.DOWN = false;
+  }
+
+  if (e.keyCode == 32) {
+    keyboard.SPACE = false;
+  }
+
+  if (e.keyCode == 68) {
+    keyboard.D = false;
+  }
+
+});
+
+function fullscreen(){
+
   let fullscreen = document.getElementById("fullscreen");
   enterFullscreen(fullscreen);
 }
