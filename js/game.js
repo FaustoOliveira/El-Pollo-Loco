6650c400a995b2f20c7a41e242ad0a1bd81a7629
Fausto Oliveira <105312418+FaustoOliveira@users.noreply.github.com>
2023-08-06 12:52:58 +0200

    updateControl

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ startScreen.src = "img/9_intro_outro_screens/start/startscreen_2.png"
  */
 function init() {
   canvas = document.getElementById("canvas");
-  ctx = canvas.getContext('2d');
+  ctx = canvas.getContext("2d");
   ctx.drawImage(startScreen, 0, 0, 720, 480);
 }
 
@@ -20,62 +20,8 @@ function init() {
 function startGame(){
   gameStared = true;
   world = new World(canvas, keyboard);
-  document.getElementById('start').style.display = 'none';
+  document.getElementById("start").style.display = "none";
 }
-
-window.addEventListener("keydown", (e) => {
-  if (e.keyCode == 39) {
-    keyboard.RIGHT = true;
-  }
-
-  if (e.keyCode == 37) {
-    keyboard.LEFT = true;
-  }
-
-  if (e.keyCode == 38) {
-    keyboard.UP = true;
-  }
-
-  if (e.keyCode == 40) {
-    keyboard.DOWN = true;
-  }
-
-  if (e.keyCode == 32) {
-    keyboard.SPACE = true;
-  }
-
-  if (e.keyCode == 68) {
-    keyboard.D = true;
-  } 
-  
-});
-
-window.addEventListener("keyup", (e) => {
-  if (e.keyCode == 39) {
-    keyboard.RIGHT = false;
-  }
-
-  if (e.keyCode == 37) {
-    keyboard.LEFT = false;
-  }
-
-  if (e.keyCode == 38) {
-    keyboard.UP = false;
-  }
-
-  if (e.keyCode == 40) {
-    keyboard.DOWN = false;
-  }
-
-  if (e.keyCode == 32) {
-    keyboard.SPACE = false;
-  }
-
-  if (e.keyCode == 68) {
-    keyboard.D = false;
-  }
-
-});
 
 function fullscreen(){
 
@@ -100,3 +46,4 @@ function exitFullscreen() {
     document.webkitExitFullscreen();
   }
 }
+gameControl();
