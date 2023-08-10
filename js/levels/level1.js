668f12ae1c3c7c0c4a79003e1ed0e88d1061e83b
Fausto Oliveira <f4_u@hotmail.de>
2023-08-10 13:20:49 +0200

    newUpdate

--- js/levels/level1.js
+++ js/levels/level1.js
@@ -10,6 +10,20 @@ const level1 = new level(
         new Cloud()
 
     ],
+
+    [
+      new Coin(200, 100),
+      new Coin(500, 100),
+      new Coin(1000, 100),
+      new Coin(1500, 100),
+      new Coin(2000, 100),
+      new Coin(2500, 100),
+      new Coin(3000, 100),
+      new Coin(3200, 100),
+      new Coin(700, 100),
+      new Coin(1200, 100)
+
+  ],
     [
     new BackgroundObject("img/5_background/layers/air.png", -719),
     new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
